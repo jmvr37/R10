5358c82d99a6e297862faa6f3dc24ea9b14c877f
jose <jmvr37@hotmail.com>
2020-01-20 16:16:04 -0800

    add schedule screen to navbar

--- js/navigation/NavigationLayout.js
+++ js/navigation/NavigationLayout.js
@@ -4,11 +4,15 @@ import {
 } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import About from '../screens/About/About';
-
+import Schedule from '../screens/Schedule/Schedule';
 
 const AboutStack = createStackNavigator({
-  About: {router: About}
+  About: {screen: About}
 });
+
+const ScheduleStack = createStackNavigator({
+  Schedule: {router: Schedule}
+})
 
 // Dedicated stacks for Schedule and Faves will go here too!
 export default createBottomTabNavigator(AboutStack);
