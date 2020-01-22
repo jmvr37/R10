bb0dfc623cbc7261b8d2788a00460cd0ffbc2c26
jose <jmvr37@hotmail.com>
2020-01-21 18:55:26 -0800

    add some code to moddels js

--- js/config/models.js
+++ js/config/models.js
@@ -1,1 +1,25 @@
+import React from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
+
+let obj = {
+    id: 1, 
+    name: "event", 
+    time: "10",
+}
+AsyncStorage.setItem('user',JSON.stringify(obj));
+
+
+export default class AddFave extends React.Component {
+
+
+saveData = async = () =>{
+try {
+    await AsyncStorage.setItem("thisISaKey", JSON.stringify({ value: "Hey!" }));
+    const result = await AsyncStorage.getItem("thisISaKey");
+  } catch (e) {
+    throw e;
+  }
+  console.log(JSON.parse(result).value);
+  
+}
+}
